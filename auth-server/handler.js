c97b188ce81d9e8954819b26fbde0282f9967468
valvegan <valentinavaccari@hotmail.com>
2022-04-26 15:11:49 +0100

    new functions
    oAuth2

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -1,18 +1,98 @@
-'use strict';
+const {google} = require("googleapis");
+const OAuth2 = google.auth.OAuth2;
+const calendar = google.calendar("v3");
+/**
+ * SCOPES allows you to set access levels, this is set to randomly for now because no access rights are present to update the calendar yourself
+ * for more info check the scopes documentation
+ */
+const SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"];
+/**
+ * credentials
+ * process.env means the value is in the config.json file. this is a best practice as it keeps the api secrets hidden
+ */
+const credentials = {
+  client_id: process.env.CLIENT_ID,
+  project_id: process.env.PROJECT_ID,
+  client_secret: process.env.CLIENT_SECRET,
+  calendar_id: process.env.CALENDAR_ID,
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  redirect_uris: ["https://valvegan.github.io/events_app/"],
+  javascript_origins: ["https://valvegan.github.io", "http://localhost:3000"],
+};
 
-module.exports.hello = async (event) => {
+const {client_secret, client_id, redirect_uris, calendar_id}=
+credentials;
+const oAuth2Client = new google.auth.OAuth2(
+  client_id,
+  client_secret,
+  redirect_uris[0]
+);
+/**
+ * The first step in the OAuth process is to generate a URL so users can log in with
+ * Google and be authorized to see your calendar. After logging in, they’ll receive a code
+ * as a URL parameter.
+ *
+ */
+module.exports.getAuthURL = async () => {
+  /**
+   *
+   * Scopes array passed to the `scope` option. Any scopes passed must be enabled in the
+   * "OAuth consent screen" settings in your project on your Google Console. Also, any passed
+   *  scopes are the ones users will see when the consent screen is displayed to them.
+   *
+   */
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: "offline",
+    scope: SCOPES,
+  });
+
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
+    body: JSON.stringify({
+      authUrl: authUrl,
+    }),
   };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
+
+module.exports.getAccessToken = async (event) => {
+  //the values used to instantiate the OAuthClient are at the top of the file
+  const oAuth2Client = new google.auth.OAuth2(
+    client_id,
+    client_secret,
+    redirect_uris[0]
+    );
+    //decode authorization code extracted from the url query
+    const code = decodeURIComponent(`
+    ${event.pathParameters.code}`);
+    return new Promise ((resolve,reject)=>{
+      /**exchange authorization code for access token with a callback after the exchange,
+       * the callback is an arrow function with the results as parameters 
+       */
+      oAuth2Client.getToken(code, (err, token)=>{
+        if(err){
+          return reject(err);
+        }
+        return resolve(token);
+      });
+    })
+    .then((token)=>{
+      //respond with oAuth token
+      return {
+        statusCode: 200,
+        body: JSON.stringify(token),
+      };
+    })
+    .catch((err)=>{
+      console.error(err);
+      return {
+        statusCode: 500,
+        body: JSON.stringify(err)
+      };
+    });
+};
+
